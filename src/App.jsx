f84825e01da9fd5628fa8b25c5a6e8081a5b536b
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-04-13 11:06:45 +0100

    fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Header  from './components/Header'
-import FeedbackItem from "./components/FeedbackItem";
+
 
 
 function App() {
 
+
   return (
-    <>
-    <Header />
     <div className="container">
-      <FeedbackItem />
+      hello
     </div>
-    </>
+
   )
 }
 
