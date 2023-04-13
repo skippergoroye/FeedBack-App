7892056b610bb4cd1da781f643d2925281dd25d5
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-04-13 11:40:15 +0100

    adding value dynamically and mapping through data

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 
-
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment One'},
+    {id: 2, text: 'Comment Two'},
+    {id: 3, text: 'Comment Three'}
+  ]
 
 
   return (
     <div className="container">
-      hello
-    </div>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
 
+
+      <div>
+        <h1>Comments: ({comments.length})</h1>
+        <ul>
+          {comments.map((eachComment, item) => (
+            <li key={item}>{eachComment.text}</li> 
+          ))}
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
