f49c096a864e5ea7b2db5889e001326a6cb6c12a
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-04-13 12:09:27 +0100

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
+  const title = "Blog Post";
+  const body = "This is my blog post";
   const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'}
-  ]
+    { id: 1, text: "Comment One" },
+    { id: 2, text: "Comment Two" },
+    { id: 3, text: "Comment Three" },
+  ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading......</h1>;
+
+
+  const commentBlock = (
+    <div>
+      <h1>Comments: ({comments.length})</h1>
+      <ul>
+        {comments.map((eachComment, item) => (
+          <li key={item}>{eachComment.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
 
   return (
     <div className="container">
@@ -19,19 +34,67 @@ function App() {
       <p>{body}</p>
 
 
-      <div>
-        <h1>Comments: ({comments.length})</h1>
-        <ul>
-          {comments.map((eachComment, item) => (
-            <li key={item}>{eachComment.text}</li> 
-          ))}
-        </ul>
-      </div>
+
+
+      {/* if show comment is true return 'yes' else return 'no' */}
+      {/* {showComments ? 'yes' : 'no'}      */}
+      {/* {showComments ? ('comment') : ('no')}      */}
+
+
+       {/* using ternary and  No */}
+      {showComments ? (
+        <div>
+          <h1>Comments: ({comments.length})</h1>
+          <ul>
+            {comments.map((eachComment, item) => (
+              <li key={item}>{eachComment.text}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        "no"
+      )}
+
+
+
+
+      {/* Using ternary and null */}
+      {showComments ? (
+        <div>
+          <h1>Comments: ({comments.length})</h1>
+          <ul>
+            {comments.map((eachComment, item) => (
+              <li key={item}>{eachComment.text}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        null
+      )}
+
+
+
+
+      {/* Using double ampersand If showComment is true then go ahead and show the comment */}
+      {showComments && (
+          <div>
+            <h1>Comments: ({comments.length})</h1>
+            <ul>
+              {comments.map((eachComment, item) => (
+                <li key={item}>{eachComment.text}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+
+
+
+       {/* Using double ampersand If showComment is true then go ahead and show the comment */}
+       {showComments && commentBlock }
 
     </div>
-  )
+  );
 }
-
-
 
 export default App;
