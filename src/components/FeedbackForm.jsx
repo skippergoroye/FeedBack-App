9c3774a52e3382f71791d8d3aa042b842e6a5586
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-04-14 16:23:26 +0100

    form validation added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,26 +4,45 @@ import Button from "./shared/Button";
 
 
 const FeedbackForm = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-  //   const handleTextChange = (e) => {
-  //     console.log(e.target.value)
-  //   }
+    const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10){
+            setMessage('Text must be at least 10 character')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
+         setText(e.target.value)
+    //   console.log(e.target.value)
+    }
+
+
 
   return (
     <Card>
       <form>
-        <h1>How would rate your service with us</h1>
-        {/* @todo -  rating select component */}
-        <div className="input-group">
-          <input
-            type="text"
-            placeholder="Write a review"
-            // onChange={handleTextChange}
-            // onClick={() => setText(console.log(123))}
-          />
-          <Button type="submit">Send</Button>
-        </div>
+            <h1>How would rate your service with us</h1>
+            {/* @todo -  rating select component */}
+            <div className="input-group">
+                <input
+                    type="text"
+                    placeholder="Write a review"
+                    onChange={handleTextChange}
+                    // onClick={() => setText(console.log(123))}
+                />
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+            </div>
+
+            // here
+            {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
