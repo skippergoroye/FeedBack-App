f205aa532cb5e70f8fea8a4db2c3c020046d8b74
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-04-14 09:39:05 +0100

    global state

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedBackData from "./Data/FeedBackData";
+
+
 
 function App () {
+  const [feedback, setFeedBack] = useState(FeedBackData)
   
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,35 +1,12 @@
 import { useState } from 'react'
 
-const FeedbackItem = () => {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This ia an eaxmple of a feddback Item')
-
-
-    // const handleClick  = () => {
-    //     setRating(10)
-    // }
-
-
-    // const handleClick  = () => {
-    //     setRating(() => {
-    //         return 10
-    //     })
-    // }
-
-
-    const handleClick  = () => {
-        setRating((prev) => {
-            // console.log(prev)
-            return prev + 1
-        })
-    }
-
+const FeedbackItem = ({ item }) => {
+   
 
   return (
     <div className='card'>
-        <div className='num-display'>{rating}</div>
-        <div className='text-display'>{text}</div>
-        <button onClick={handleClick}>Click</button>
+        <div className='num-display'>{item.rating}</div>
+        <div className='text-display'>{item.text}</div>
     </div>
   )
 }
